d66784c4605c5af66d0011ab460dc3abce49243b
dorasnowflake <122491558+dorasnowflake@users.noreply.github.com>
2023-01-26 16:10:43 -0600

    Update streamlit_app.py
    added reference to pandas
    used pandas to read fruit list csv
    used pandas to display contents of fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,7 @@ streamlit.text('\N{chicken}\N{egg} Hard-Boiled Free-Range Egg')
 streamlit.text('\N{avocado}\N{bread} Avocado Toast')
 
 streamlit.header('\N{banana}\N{strawberry} Build Your Own Fruit Smoothie \N{kiwifruit}\N{mango}')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
