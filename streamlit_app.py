2995f2a067fc5784b4a9c6695003617b46fad779
dorasnowflake <122491558+dorasnowflake@users.noreply.github.com>
2023-01-26 09:07:34 -0600

    Update streamlit_app.py
    added second header

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,6 @@ streamlit.header('Breakfast Favorites')
 streamlit.text('\N{blueberries}\N{bowl with spoon} Omega 3 & Blueberry Oatmeal')
 streamlit.text('\N{leafy green}\N{rocket} Kale, Spinach & Rocket Smoothie')
 streamlit.text('\N{egg} Hard-Boiled Free-Range Egg')
+streamlit.text('\N{avocado}\N{toast} Hard-Boiled Free-Range Egg')
+
+streamlit.header('\N{banana}\N{strawberry} Build Your Own Fruit Smoothie\N{kiwi fruit}\N{mango}')
