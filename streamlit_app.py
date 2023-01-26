3db2509d34e7fb0c1da733598423998e47b299c0
dorasnowflake <122491558+dorasnowflake@users.noreply.github.com>
2023-01-26 09:03:14 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.title('My Mom\'s New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('\N{blueberries}\N{bowl with spoon} Omega 3 & Blueberry Oatmeal')
-
-
+streamlit.text('\N{kale}\N{rocket} Kale, Spinach & Rocket Smoothie')
+streamlit.text('\N{egg}\N{rocket} Hard-Boiled Free-Range Egg')
