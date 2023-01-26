cff9dc20bc07e72a2c0bcfd77e0d3368b15842ca
dorasnowflake <122491558+dorasnowflake@users.noreply.github.com>
2023-01-26 08:56:22 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My Mom\'s New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
-streamlit.text('\N{arm} Omega 3 & Blueberry Oatmeal')
+streamlit.text('\N{banana} Omega 3 & Blueberry Oatmeal')
 
 
